9191bcfc744e04374c9126ac476f74d5784a02c4
Jeferson Pires <jeff_ap@hotmail.com>
2023-12-20 20:31:20 -0300

    Entrega projeto final

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,8 +4,10 @@ const app = express();
 app.use(express.json())
 app.listen(3333);
 
+
 require('dotenv').config()
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+console.log("API OK")
 
 //CORS
 const cors = require('cors');
@@ -15,9 +17,15 @@ app.options('*', cors());
 // OPEN-AI
 const {Configuration, OpenAIApi} = require("openai");
 const config = new Configuration({
-    apiKey: OPENAI_API_KEY,
+    apiKey: OPENAI_API_KEY, 
 });
 const openai = new OpenAIApi(config);
+
+
+// GET DE TESTE
+app.get('/api/call', (request,response) => {
+    return response.send({'message': 'Chat GPT'});
+});
 
 
 // POST
@@ -27,8 +35,8 @@ app.post('/api/call', async (req, res) =>{
       const response = await openai.createCompletion({
           model: "text-davinci-003",
           prompt: req.body.prompt,
-          max_tokens: 1000,
-          temperature: 5,
+          max_tokens: 1024,
+          temperature: 0.8,
         });
         return response.data;
   };
